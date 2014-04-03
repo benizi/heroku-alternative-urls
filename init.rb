508e1a4deb241328bc35c6a254389ce38d09b55e
Benjamin R. Haskell <heroku@benizi.com>
2014-04-03 01:29:20 -0400

    Adds same `.git` existence check as in base
    Don't use the `git()` helper if `.git` doesn't exist.

--- init.rb
+++ init.rb
@@ -4,6 +4,8 @@ class Heroku::Command::Base
   def git_remotes(base_dir=Dir.pwd)
     remotes = original_git_remotes(base_dir)
     Dir.chdir(base_dir) do
+      return unless File.exists?('.git')
+
       git('remote -v').split(/\n/).each do |line|
         name, url, method = line.split
         if url =~ /^(?:git@)?he(?:roku\.com|-\w+)?:([\w\d-]+)(?:\.git)?$/
