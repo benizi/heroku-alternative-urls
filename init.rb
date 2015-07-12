837d22d0b1430b67a15c412ecc4b9dfd35c4c2b8
Benjamin R. Haskell <heroku@benizi.com>
2015-07-12 17:24:37 -0400

    Fix bug in newer Heroku toolbelt versions
    `original_git_remotes` was returning `nil` instead of a hash. Initialize
    it to an empty hash if it's `nil`.

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ class Heroku::Command::Base
   alias :original_git_remotes :git_remotes
 
   def git_remotes(base_dir=Dir.pwd)
-    remotes = original_git_remotes(base_dir)
+    remotes = original_git_remotes(base_dir) || {}
     Dir.chdir(base_dir) do
       return unless File.exists?('.git')
 
